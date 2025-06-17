233908c8af3d815a2de97c1da4fe59e58fede1dc
root <root@v302238910.local>
2025-06-17 13:42:12 +0000

    input validation

--- setup.sh
+++ setup.sh
@@ -5,6 +5,7 @@ set -e
 apt update && apt install -y curl openssl
 bash -c "$(curl -L https://github.com/XTLS/Xray-install/raw/main/install-release.sh)" @ install
 systemctl enable xray
+systemctl stop xray
 
 # set variables
 UUID=$(xray uuid)
@@ -19,16 +20,58 @@ while true; do
 done
 PUBLIC_IP=$(curl -s ipinfo.io/ip)
 clear
-read -p "External IP of this server (press Enter to use ${PUBLIC_IP}): " SERVER_IP
-SERVER_IP=${SERVER_IP:-${PUBLIC_IP}}
-read -p "Reality server address (press Enter for default: www.yahoo.com): " SNI
-SNI=${SNI:-'www.yahoo.com'}
-read -p "Shadowsocks port (press Enter for default: 8888): " SS_PORT
-SS_PORT=${SS_PORT:-8888}
+while true; do
+  read -p "External IP of this server (press Enter to use ${PUBLIC_IP}): " SERVER_IP
+  SERVER_IP=${SERVER_IP:-${PUBLIC_IP}}
+  if [[ $SERVER_IP =~ ^[0-9.]+$ ]]; then
+    break
+  else
+    echo "Error: please enter a valid IP address."
+  fi
+done
+echo
+while true; do
+  read -p "VLESS port (press Enter for default: 443): " VLESS_PORT
+  VLESS_PORT=${VLESS_PORT:-443}
+  if ! [[ $VLESS_PORT =~ ^[0-9]+$ ]]; then
+    echo "Error: please enter a valid port."
+    continue
+  fi
+  if ss -tln | grep -qw ":$VLESS_PORT "; then
+    echo "Error: port is busy. Try another."
+    continue
+  fi
+  break
+done
+echo
+while true; do
+  read -p "Reality server address (press Enter for default: www.yahoo.com): " SNI
+  SNI=${SNI:-'www.yahoo.com'}
+  if ! [[ $SNI =~ .*[0-9\W]$ ]]; then
+    break
+  else
+    echo "Error: please enter a valid domain address."
+  fi
+done
+echo
+while true; do
+  read -p "Shadowsocks port (press Enter for default: 8888): " SS_PORT
+  SS_PORT=${SS_PORT:-8888}
+  if ! [[ $SS_PORT =~ ^[0-9]+$ ]]; then
+    echo "Error: please enter a valid port."
+    continue
+  fi
+  if ss -tln | grep -qw ":$SS_PORT "; then
+    echo "Error: port is busy. Try another."
+    continue
+  fi
+  break
+done
 
 # prepare config file
 cp ./config.json.template /usr/local/etc/xray/config.json
 sed -i "s|SERVER_IP|${SERVER_IP}|g" /usr/local/etc/xray/config.json
+sed -i "s|VLESS_PORT|${VLESS_PORT}|g" /usr/local/etc/xray/config.json
 sed -i "s|UUID|${UUID}|g" /usr/local/etc/xray/config.json
 sed -i "s|PRIVATE_KEY|${PRIVATE_KEY}|g" /usr/local/etc/xray/config.json
 sed -i "s|SS_PASS|${SS_PASS}|g" /usr/local/etc/xray/config.json
@@ -38,17 +81,23 @@ sed -i "s|SNI|${SNI}|g" /usr/local/etc/xray/config.json
 # apply settings
 systemctl restart xray
 sleep 1
-echo "Xray status:"
-systemctl status xray | grep Active
+echo
+if systemctl status xray | grep -q active; then
+  echo "Xray status:"
+  systemctl status xray | grep Active
+else
+  echo "Error: service did't start. Try another domain or ports"
+  exit 1
+fi
 
 # Get connection strings
-echo ""
+echo
 echo "Connection details saved to connect.txt:"
-echo ""
+echo
 echo "VLESS:" > connect.txt
-echo "vless://${UUID}@${SERVER_IP}:443/?encryption=none&type=tcp&sni=${SNI}&fp=chrome&security=reality&alpn=h2&flow=xtls-rprx-vision&pbk=${PUBLIC_KEY}&packetEncoding=xudp" >> connect.txt
-echo "" >> connect.txt
+echo "vless://${UUID}@${SERVER_IP}:${VLESS_PORT}/?encryption=none&type=tcp&sni=${SNI}&fp=chrome&security=reality&alpn=h2&flow=xtls-rprx-vision&pbk=${PUBLIC_KEY}&packetEncoding=xudp" >> connect.txt
+echo >> connect.txt
 echo "Shadowsocks-2022:" >> connect.txt
 echo "ss://2022-blake3-aes-128-gcm:${SS_PASS}@${SERVER_IP}:${SS_PORT}" >> connect.txt
 cat connect.txt
-echo ""
+echo
